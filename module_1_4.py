faa6c451b6ab89d130b397199760500091507453
buninegor95 <buninegor95@gmail.com>
2024-10-18 21:48:19 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+Number_of_completed_homework = 12
+Number_of_hours_spent = 1.5 * 60
+Course_name = 'Python'
+Time_per_task = int(Number_of_hours_spent) / Number_of_completed_homework
+print(f"количество работ: {Number_of_completed_homework}")
+print(f"время потраченое на работы в минутах: {Number_of_hours_spent}")
+print(f"название курса: {Course_name}")
+print(f"на 1 задание затрачено минут: {Time_per_task}")
